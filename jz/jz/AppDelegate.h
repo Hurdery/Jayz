c802352e0cc3cc4cb21824b9093840315c4f003b
介子 <yimian_123@icloud.com>
2018-03-23 18:03:51 +0800

    gg

---
+++ jz/jz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  jz
+//
+//  Created by leonardo on 2018/3/23.
+//  Copyright © 2018年 leonardo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ jz/jz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  jz
+//
+//  Created by leonardo on 2018/3/23.
+//  Copyright © 2018年 leonardo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
