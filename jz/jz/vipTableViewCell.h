99ff231ae6f5aeb8175549967fa05544b9cd4bc3
Hurdery <2522905745@qq.com>
2018-03-26 19:23:02 +0800

    init

---
+++ jz/jz/vipTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  vipTableViewCell.h
+//  jz
+//
+//  Created by leonardo on 2018/3/26.
+//  Copyright © 2018年 leonardo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface vipTableViewCell : UITableViewCell
+
+@end
---
+++ jz/jz/vipTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  vipTableViewCell.m
+//  jz
+//
+//  Created by leonardo on 2018/3/26.
+//  Copyright © 2018年 leonardo. All rights reserved.
+//
+
+#import "vipTableViewCell.h"
+
+@implementation vipTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
