ac6540db56c1526c52c63bdaf90a9cca6407307d
Hurdery <2522905745@qq.com>
2018-03-26 18:10:57 +0800

    pmsix

--- jz/jz/ViewController.m
+++ jz/jz/ViewController.m
@@ -18,8 +18,13 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
-
-
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    NSLog(@"2133");
+}
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    NSLog(@"下午六点该吃饭");
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
